9ea15ee63ea2ddfc433ee16d853f3e4b47db5ed9
Rodica-Elena Andronache <rodica.andronache@vertistudio.com>
2017-04-20 13:59:36 +0300

    Update version

--- pirate-switch.php
+++ pirate-switch.php
@@ -10,7 +10,7 @@
  * Plugin Name:       Pirate Switch
  * Plugin URI:        -
  * Description:       A simple, intuitive, and powerful plugin that allows you to add a style switcher on your site.
- * Version:           1.0.2
+ * Version:           1.0.3
  * Author:            Themeisle
  * Author URI:        themeisle.com
  * License:           GPL-2.0+
@@ -31,7 +31,7 @@ if ( ! defined( 'PIRATE_SWITCH_PATH' ) ) {
 
 /* Version */
 if ( ! defined( 'PIRATE_SWITCH_VERSION' ) ) {
-define("PIRATE_SWITCH_VERSION","1.0.2");
+define("PIRATE_SWITCH_VERSION","1.0.3");
 }
 
 /* URL */
