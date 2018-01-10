30a7705ede50881f9b64490ce05c98ba77671b9e
Andrei Baicus <baicusandrei@me.com>
2018-01-10 22:00:46 +0200

    Fixes and closes #31. Changed behaviour based on device on load.

--- public/js/pirate-switch-public.js
+++ public/js/pirate-switch-public.js
@@ -1,39 +1,45 @@
+/* global jQuery, self */
 jQuery( document ).ready( function ( $ ) {
+    "use strict";
+	$( 'head' ).append( '<style class="pirate_switch_css_container"></style>' );
 
-    if( window.location.hash.substr(1) === 'switcher-open' ) {
-        toggleSwitcher();
-    }
-    $( '#ps-open-icon' ).on( "click", function () {
-        toggleSwitcher();
-    } );
+	if ( $( window ).innerWidth() > '1024' ) {
+		toggleSwitcher();
+	}
 
-    $( 'head' ).append( '<style class="pirate_switch_css_container"></style>' );
+	if ( window.location.hash.substr( 1 ) === 'switcher-open' ) {
+		toggleSwitcher();
+	}
+	$( '#ps-open-icon' ).on( "click", function () {
+		toggleSwitcher();
+	} );
 
-    var cssContainer = $( '.pirate_switch_css_container' );
+	var cssContainer = $( '.pirate_switch_css_container' );
 
-    $( '.ps-color-box' ).on( "click", function () {
-        $(this).addClass('ps-color-effect');
-        setTimeout(
-            function() {
-                $('.ps-color-effect').removeClass('ps-color-effect');
-            },
-            250);
+	$( '.ps-color-box' ).on( "click", function () {
+		$( this ).addClass( 'ps-color-effect' );
+		setTimeout(
+			function () {
+				$( '.ps-color-effect' ).removeClass( 'ps-color-effect' );
+			},
+			250 );
 
-    $( cssContainer ).empty();
+		$( cssContainer ).empty();
 
-        var cssCode = jQuery( this ).next().val();
+		var cssCode = jQuery( this ).next().val();
 
-        if ( (cssCode != 'undefined') && (cssCode != '') )
-            $( cssContainer ).append( cssCode );
-    } );
+		if ( (cssCode != 'undefined') && (cssCode != '') )
+			$( cssContainer ).append( cssCode );
+	} );
 
-    if( self!==top ) {
-        $('.ps-button-cta').remove();
-    }
+	if ( self !== top ) {
+		$( '.ps-button-cta' ).remove();
+	}
 } );
 
 function toggleSwitcher() {
-    var $switchMainBox = jQuery( '#ps-main-box, #ps-open-icon' ),
-        hideClass = 'ps-opened';
-    $switchMainBox.toggleClass( hideClass );
+    "use strict";
+	var $switchMainBox = jQuery( '#ps-main-box, #ps-open-icon' ),
+		hideClass = 'ps-opened';
+	$switchMainBox.toggleClass( hideClass );
 }
